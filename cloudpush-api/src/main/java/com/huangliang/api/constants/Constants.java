72a819abc632cba959ae7bdb03a31e66ee4a7b9c
huangliang <hl7226261@2980.com>
2019-03-15 16:48:41 +0800

    增加产量

--- cloudpush-api/src/main/java/com/huangliang/api/constants/Constants.java
+++ cloudpush-api/src/main/java/com/huangliang/api/constants/Constants.java
@@ -4,4 +4,16 @@ public interface Constants {
     String WEBSOCKET_SERVER = "websocket";
 
     String ROCKETMQ_TOPIC_PREFIX = "websocket";
+
+    String ROCKETMQ_GROUP_PREFIX = "group";
+
+    String WEBSOCKET = "websocket";
+
+    String UPGRADE = "Upgrade";
+
+    String POST = "POST";
+
+    String GET = "GET";
+
+    String CHANNELID = "channelId";
 }
