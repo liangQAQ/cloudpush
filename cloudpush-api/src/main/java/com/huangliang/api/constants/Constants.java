3d4d43c3cb67c7ae6b604dbb90b5c3aa8941a6b8
huangliang <hl7226261@2980.com>
2019-03-21 21:01:36 +0800

    新增常量

--- cloudpush-api/src/main/java/com/huangliang/api/constants/Constants.java
+++ cloudpush-api/src/main/java/com/huangliang/api/constants/Constants.java
@@ -11,9 +11,55 @@ public interface Constants {
 
     String UPGRADE = "Upgrade";
 
+    String SYSTEM = "System";
+
     String POST = "POST";
 
     String GET = "GET";
 
     String CHANNELID = "channelId";
+
+    enum MessageType {
+        //服务端向客户端推送类型
+        SEND("SEND"),
+        //客户端收到消息回执类型
+        ACK("ACK"),
+        //心跳类型
+        HEARTBEAT("HEARTBEAT");
+
+        String value;
+
+        MessageType(String s) {
+            this.value = s;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+
+    enum MessageTrigger {
+        //客户端收到消息回执类型
+        HTTP("HTTP"),
+        //心跳类型
+        WEBSOCKET("WEBSOCKET");
+
+        String value;
+
+        MessageTrigger(String s) {
+            this.value = s;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
 }
