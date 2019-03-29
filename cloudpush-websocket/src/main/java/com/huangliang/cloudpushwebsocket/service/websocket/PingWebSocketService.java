2f7901ce74650f339b42641695853e6505fa3bb1
huangliang <hl7226261@2980.com>
2019-03-29 17:07:36 +0800

    对应客户端ping请求返回pong

--- cloudpush-websocket/src/main/java/com/huangliang/cloudpushwebsocket/service/websocket/PingWebSocketService.java
+++ cloudpush-websocket/src/main/java/com/huangliang/cloudpushwebsocket/service/websocket/PingWebSocketService.java
@@ -1,6 +1,8 @@
 package com.huangliang.cloudpushwebsocket.service.websocket;
 
+import com.huangliang.cloudpushwebsocket.constants.Constants;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.websocketx.PongWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Service;
 public class PingWebSocketService implements IWebSocketService{
     @Override
     public void handler(ChannelHandlerContext ctx,WebSocketFrame frame) {
-        log.info("Ping来了。。。。");
+        log.info("[{}]Ping来了。。。。",ctx.channel().attr(Constants.attrChannelId).get());
+        ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+        return ;
     }
 }
