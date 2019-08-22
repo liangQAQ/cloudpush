fa0634c16a65cce1a6cc55278136221bca00a472
huangliang <hl7226261@2980.com>
2019-08-22 19:41:08 +0800

    添加请求标识字段

---
+++ cloudpush-api/src/main/java/com/huangliang/api/entity/request/SendRequest.java
@@ -0,0 +1,29 @@
+package com.huangliang.api.entity.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@ApiModel(value="推送请求对象模型")
+public class SendRequest extends AbstractRequest{
+
+    /**
+     * 发送目的地(客户端)
+     */
+    @ApiModelProperty(value="推送目的地的数组" ,required=true)
+    private List<String> to;
+    /**
+     * 发送内容
+     */
+    @ApiModelProperty(value="推送消息的内容" ,required=true)
+    private String msg;
+
+    @ApiModelProperty(value="延时推送(单位秒,暂未实现)" ,required=false)
+    private Integer delay;
+
+    @ApiModelProperty(value="是否发送给所有人" ,required=false)
+    private Boolean sendToAll = false;
+}
