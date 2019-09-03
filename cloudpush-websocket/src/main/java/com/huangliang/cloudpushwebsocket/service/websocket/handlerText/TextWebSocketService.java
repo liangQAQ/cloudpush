f71da9d229137d04bace1fd1ba75c25df9af18c1
huangliang <hl7226261@2980.com>
2019-09-03 21:02:48 +0800

    增加websocket请求，来触发推送的逻辑

---
+++ cloudpush-websocket/src/main/java/com/huangliang/cloudpushwebsocket/service/websocket/handlerText/TextWebSocketService.java
@@ -0,0 +1,52 @@
+package com.huangliang.cloudpushwebsocket.service.websocket.handlerText;
+
+import com.alibaba.fastjson.JSONObject;
+import com.huangliang.api.entity.WebsocketMessage;
+import com.huangliang.cloudpushwebsocket.constants.Constants;
+import com.huangliang.cloudpushwebsocket.service.websocket.IWebSocketService;
+import com.huangliang.cloudpushwebsocket.service.websocket.handlerText.MessageServiceStrategy;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 处理客户端发过来的推送消息
+ */
+@Service
+@Slf4j
+public class TextWebSocketService implements IWebSocketService {
+
+    @Autowired
+    private MessageServiceStrategy messageServiceStrategy;
+
+    @Override
+    public void handler(ChannelHandlerContext ctx, WebSocketFrame frame) {
+        String str = ((TextWebSocketFrame) frame).text();
+        if (StringUtils.isEmpty(str)) {
+            return ;
+        }
+        Channel channel = ctx.channel();
+        log.info("receive[{}]:" + str,channel.attr(Constants.attrChannelId).get());
+        //按规定规则解析消息
+        WebsocketMessage msg = init(str);
+        if(msg == null ){
+            return ;
+        }
+        //处理消息
+        messageServiceStrategy.handler(channel,msg);
+    }
+
+    private WebsocketMessage init(String str) {
+        try {
+            return JSONObject.parseObject(str, WebsocketMessage.class);
+        } catch (Exception e) {
+            log.error("json解析失败,无法识别的消息", e);
+            return null;
+        }
+    }
+}
