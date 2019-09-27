f91492cead519a9ca92930b5e716691784f5c7ae
huangliang <hl7226261@2980.com>
2019-09-27 15:13:45 +0800

    删除多余代码

---
+++ cloudpush-task/src/main/java/com/huangliang/cloudpushtask/CloudpushTaskApplication.java
@@ -0,0 +1,13 @@
+package com.huangliang.cloudpushtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudpushTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudpushTaskApplication.class, args);
+    }
+
+}
