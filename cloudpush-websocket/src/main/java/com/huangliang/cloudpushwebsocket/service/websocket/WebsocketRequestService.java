997e2bd1a5228d6310588c542deb063c6f35eaf7
lianghuang6 <lianghuang6@iflytek.com>
2020-12-02 17:18:39 +0800

    优化保活策略

--- cloudpush-websocket/src/main/java/com/huangliang/cloudpushwebsocket/service/websocket/WebsocketRequestService.java
+++ cloudpush-websocket/src/main/java/com/huangliang/cloudpushwebsocket/service/websocket/WebsocketRequestService.java
@@ -1,5 +1,6 @@
 package com.huangliang.cloudpushwebsocket.service.websocket;
 
+import com.huangliang.cloudpushwebsocket.service.channel.ChannelService;
 import com.huangliang.cloudpushwebsocket.service.websocket.WebsocketServiceStrategy;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.websocketx.*;
@@ -14,8 +15,14 @@ public class WebsocketRequestService {
     @Autowired
     private WebsocketServiceStrategy websocketServiceStrategy;
 
+    @Autowired
+    private ChannelService channelService;
+
     public void handler(ChannelHandlerContext ctx, WebSocketFrame frame) {
         try {
+            //收到消息，先更新维护客户端的活跃时间
+            channelService.updateActiveTime(ctx.channel());
+            //处理消息
             websocketServiceStrategy.execute(ctx,frame);
         } catch (Exception e) {
             log.error("处理消息异常", e);
--- cloudpush-websocket/src/main/java/com/huangliang/cloudpushwebsocket/service/websocket/handlerPing/PingWebSocketService.java
+++ cloudpush-websocket/src/main/java/com/huangliang/cloudpushwebsocket/service/websocket/handlerPing/PingWebSocketService.java
@@ -24,7 +24,5 @@ public class PingWebSocketService implements IWebSocketService {
         log.info("[{}]Ping来了。。。。",channel.attr(AttrConstants.channelId).get());
         //写回pong响应
         ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
-        //更新活跃时间
-        channelService.updateActiveTime(channel);
     }
 }
